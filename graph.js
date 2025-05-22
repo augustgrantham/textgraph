ec29bc80d3f21529bf7b29d6370bf295e1f00a59
augustgrantham <august.grantham@gmail.com>
2025-05-21 20:18:47 -0500

    Add graph table generator 5-21-25 8:21PM
    Rudimentory graph table generator
    Uses basic nested for loop incremementing down
    Makes 20x20 table of boxes with coordinates
    correctly labeled within.
    Also did minor tweaks to css to prevent graph
    numbers from being as large as body text.
    Added graph window div and table to index.html

--- graph.js
+++ graph.js
@@ -1,6 +1,20 @@
 const button = document.getElementById("graphBtn");
-const p = document.getElementById("content");
-
+const check = document.getElementById("test");
+const graphsheet = document.getElementById("graphsheet");
 function graph() {
-    p.innerHTML = "button fucking clicked";
+    let print = "<table>";
+    let empty = " ";
+    for(let y = 20; y >= 0; y--) {
+        console.log(y);
+        print += "<tr>";
+        for(let x = 0; x <= 20; x++)
+        {
+            print += "<td>(" + x + "," + y + ")</td>";
+        }
+        print += "</td>";
+    }
+    check.innerHTML = "button fucking clicked";
+    graphsheet.innerHTML = print;
+    
+
 }
