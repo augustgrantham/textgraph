0fc0f7595f3e4118ad07f9eb9396b49f99b530e8
augustgrantham <august.grantham@gmail.com>
2025-07-10 16:09:21 -0500

    Add different function types Added linear, quadratic, exponential and absolute value functions as different options from dropdown menu.

--- graph.js
+++ graph.js
@@ -2,8 +2,16 @@
 const button = document.getElementById("graphBtn");
 const check = document.getElementById("test");
 const graphsheet = document.getElementById("graphsheet");
+const funct = document.getElementById("dropbtn");
+
+
+
 //javascript variables
 let print;
+let type = "linear";
+let a = 1;
+let c = 0;
+let func;
 function graph() {
     print = "<table>";
     let empty = " ";
@@ -11,7 +19,21 @@ function graph() {
         print += "<tr>";
         for(let x = -30; x <= 30; x++)
         {
-            let func = (x**2);
+          switch(type)
+          {
+            case "linear":
+              func = x;
+              break;
+            case "quadratic":
+              func = (x**2);
+              break;
+            case "exponential":
+              func = (2**x);
+              break;
+            case "absolute":
+              func = (Math.abs(x));
+              break;
+          }
             if(y == func) {
                 print += "<td id=\"point\"></td>";
             }
@@ -30,7 +52,24 @@ function graph() {
     }
     updateGraph();
 }
-
+function linear()
+{
+  type = "linear";
+  funct.innerHTML = "Linear";
+}
+function quadratic()
+{
+  type = "quadratic";
+  funct.innerHTML = "Quadratic";
+}
+function exponential() {
+  type = "exponential";
+  funct.innerHTML = "Exponential";
+}
+function absolute() {
+  type = "absolute";
+  funct.innerHTML = "Absolute";
+}
 function updateGraph() {
     check.innerHTML = "Graph created!";
     graphsheet.innerHTML = print;
@@ -38,4 +77,22 @@ function updateGraph() {
     graphsheet.scrollTop = 300;
     
 
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
