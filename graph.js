4dd19043a26730373e72662f2cd97e076561ac05
augustgrantham <august.grantham@gmail.com>
2025-07-11 13:37:27 -0500

    Add different color themes

--- graph.js
+++ graph.js
@@ -3,7 +3,9 @@ const button = document.getElementById("graphBtn");
 const check = document.getElementById("test");
 const graphsheet = document.getElementById("graphsheet");
 const funct = document.getElementById("dropbtn");
-
+evilIcon = document.getElementById("evil");
+//css elements
+let theme = document.querySelector(":root");
 
 
 //javascript variables
@@ -52,6 +54,7 @@ function graph() {
     }
     updateGraph();
 }
+//function types
 function linear()
 {
   type = "linear";
@@ -71,18 +74,68 @@ function absolute() {
   funct.innerHTML = "Absolute";
 }
 function updateGraph() {
+    
     check.innerHTML = "Graph created!";
     graphsheet.innerHTML = print;
     graphsheet.scrollLeft = 250;
     graphsheet.scrollTop = 300;
-    
+}
 
+//styling
+function green()
+{
+thankGodNotEvil();  
+setPrimaryColor("rgba(168, 244, 172, 0.719)");
+setSecondaryColor("rgba(116, 182, 119, 0.671)");
+setScrollColor("rgb(96, 114, 88)");
+}
+function blue()
+{
+thankGodNotEvil();
+setPrimaryColor("rgba(168, 239, 244, 0.719)");
+setSecondaryColor("rgba(116, 171, 182, 0.671)");
+setScrollColor("rgb(88, 101, 114)");
+}
+function purple()
+{
+thankGodNotEvil();
+setPrimaryColor("rgba(197, 168, 244, 0.719)");
+setSecondaryColor("rgba(182, 116, 155, 0.671)");
+setScrollColor("rgb(114, 88, 104)");
+}
+function evil()
+{
+uhOhEvil();
+setPrimaryColor("rgba(244, 168, 168, 0.719)");
+setSecondaryColor("rgba(182, 116, 119, 0.671)");
+setScrollColor("rgb(114, 88, 88)");
+}
+function setPrimaryColor(color)
+{
+  theme.style.setProperty("--primary", color);
+
+}
+function setSecondaryColor(color)
+{
+  theme.style.setProperty("--secondary", color);
+
+}
+function setScrollColor(color)
+{
+  theme.style.setProperty("--scroll", color);
 }
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function uhOhEvil() {
+  evilIcon.style.display = "block";
+}
+
+function thankGodNotEvil() {
+ evilIcon.style.display = "none";
+}
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
